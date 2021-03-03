1fb28801aca2fb87974b0bfccc430806f445d168
manhnh251995 <41736966+manhnh251995@users.noreply.github.com>
2021-03-03 15:01:01 +0700

    Update sendEmail.groovy

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -45,7 +45,7 @@ def call(RunWrapper currentBuild, List<String> emailList) {
             recipientProviders: [[$class: "RequesterRecipientProvider"]],
             to: emailList.join(", "),
             subject: "\$DEFAULT_SUBJECT",
-            body: "\$DEFAULT_CONTENT"
+            body: "\MANHNH $DEFAULT_CONTENT"
         )
     }
 }
